08a33b2d5a7fdc93bf7304b09db043e45a2a4844
Christopher Cardoso <cardoso.christopher01@gmail.com>
2019-05-29 16:47:16 +0200

    make it better

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://afternoon-depths-58172.herokuapp.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -111,7 +111,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://afternoon-depths-58172.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
